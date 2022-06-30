591e1f3fb7be19b14a0ab13e053cd7ea88608b32
kbrellas <kostasmprls@gmail.com>
2022-06-30 10:45:40 +0300

    Model Classes

---
+++ src/main/java/gr/upatras/library/models/Category.java
@@ -0,0 +1,45 @@
+package gr.upatras.library.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private List<Product> products = new ArrayList<>();
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
---
+++ src/main/java/gr/upatras/library/models/Product.java
@@ -0,0 +1,64 @@
+package gr.upatras.library.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String pname;
+    private String batchno;
+    private double price;
+    private int noofproduct;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getBatchno() {
+        return batchno;
+    }
+
+    public void setBatchno(String batchno) {
+        this.batchno = batchno;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNoofproduct() {
+        return noofproduct;
+    }
+
+    public void setNoofproduct(int noofproduct) {
+        this.noofproduct = noofproduct;
+    }
+
+    public Product(int id, String pname, String batchno, double price, int noofproduct) {
+        this.id = id;
+        this.pname = pname;
+        this.batchno = batchno;
+        this.price = price;
+        this.noofproduct = noofproduct;
+    }
+}
